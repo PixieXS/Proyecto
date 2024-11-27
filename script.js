0b40a52287168d18ff5ff9416ee60aa94b44f741
Carlos_Sagastume <care_sagastumea@unicah.edu>
2024-11-26 18:32:00 -0600

    Agua Para el Pueblo (APP)

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// Seleccionamos todos los enlaces del menú
-const navLinks = document.querySelectorAll("nav ul li a");
-
-// Obtenemos la URL actual de la página
-const currentUrl = window.location.href;
-
-// Recorremos los enlaces
-navLinks.forEach(link => {
-    // Comparamos la URL del enlace con la URL actual
-    if (link.href === currentUrl) {
-        link.classList.add("active"); // Si coinciden, añadimos la clase 'active'
-    }
-});
 
 document.addEventListener('DOMContentLoaded', function () {
     // Manejo de la visibilidad de los campos de tarjeta de crédito
